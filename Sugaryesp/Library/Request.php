6da313ae4683b0633bf9341a63b71a7f9e9be4a0
Alex <1414818093@qq.com>
2020-08-19 15:33:50 +0800

    添加忽略文件

--- Sugaryesp/Library/Request.php
+++ Sugaryesp/Library/Request.php
@@ -7,12 +7,6 @@ namespace Sugaryesp\Library;
 class Request
 {
 
-    protected $allowType = [
-        'html', 'json',
-    ];
-
-    protected $defaultType = 'html';
-
     /**
      * 当前请求的资源需要以什么格式返回出去
      *
@@ -31,9 +25,7 @@ class Request
             $type = $_POST['app'];
         }
 
-        if (!empty($type) && in_array($type, $this->allowType)) return $type;
-
-        return $this->defaultType;
+        return $type;
     }
 
 }
